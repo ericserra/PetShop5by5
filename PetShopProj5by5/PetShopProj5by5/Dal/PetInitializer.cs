6af0ea1839b4d74caf2f9fb71696a2f80ad4cf58
TalitaHigino <talitahigino@hotmail.com>
2021-03-25 11:38:29 -0300

    Front

--- PetShopProj5by5/PetShopProj5by5/Dal/PetInitializer.cs
+++ PetShopProj5by5/PetShopProj5by5/Dal/PetInitializer.cs
@@ -7,7 +7,7 @@ using PetShopProj5by5.Models;
 
 namespace PetShopProj5by5.Dal
 {
-    public class ServicesInitializer : DropCreateDatabaseIfModelChanges<PetShopContext>
+    public class PetInitializer : DropCreateDatabaseIfModelChanges<PetShopContext>
     {
         protected override void Seed(PetShopContext context)
         {
@@ -18,6 +18,14 @@ namespace PetShopProj5by5.Dal
                 new Services{ ServiceType = 3, TotalValue = 100, ServiceDescription = "Banho e Tosa" }
             };
             services.ForEach(s => context.Services.Add(s));
+            context.SaveChanges();
+            var product = new List<Product>
+            {
+                new Product{ Type = "Ração",  ProductDescription = "Ração para cachorro de frango ", IndividualValue = 4.99M},
+                new Product{ Type = "Brinquedo" ,  ProductDescription = "Bolinha de plástico ", IndividualValue = 6.99M },
+                new Product{ Type = "Petisco" , ProductDescription = "Ossinho para limpeza bucal do animal", IndividualValue = 14.99M }
+            };
+            product.ForEach(p => context.Products.Add(p));
             context.SaveChanges();
         }     
     }
