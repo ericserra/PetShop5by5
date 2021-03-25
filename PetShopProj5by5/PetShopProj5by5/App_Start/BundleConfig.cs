af8d617a0772aae62f8ac6519bd2475b75a3a61b
ericserra <ericserra.souza@gmail.com>
2021-03-25 14:50:40 -0300

    mais recente back-end
    mais recente do back

--- PetShopProj5by5/PetShopProj5by5/App_Start/BundleConfig.cs
+++ PetShopProj5by5/PetShopProj5by5/App_Start/BundleConfig.cs
@@ -25,6 +25,13 @@ namespace PetShopProj5by5
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //js    
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                      "~/Scripts/jquery-ui-{version}.js"));
+
+            //css    
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                   "~/Content/jquery-ui.css"));
         }
     }
 }
--- PetShopProj5by5/PetShopProj5by5/Models/Schedule.cs
+++ PetShopProj5by5/PetShopProj5by5/Models/Schedule.cs
@@ -4,16 +4,27 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PetShopProj5by5.Models
 {
     public class Schedule
     {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
         public DateTime AppointmentData { get; set; }
+         
         public TimeSpan AppointmentTime { get; set; }
         [Key]
         public int AppointmentProtocol { get; set; }
-        public string PaymentMethod { get; set; }       
+        public string PaymentMethod { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        [ForeignKey("Pet")]
+        public int PetID { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeID { get; set; }
+        [ForeignKey("Services")]
+        public int ServicesID { get; set; }
         public virtual Customer Customer { get; set; }      
         public virtual Pet Pet { get; set; }       
         public virtual Employee Employee { get; set; }       
